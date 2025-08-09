797f995ec737381416de61f6e7a5eb75308ddb48
GeraldoRiberia <adisankar.lalan123@gmail.com>
2025-08-10 01:55:04 +0530

    Revert "fix for merge"
    This reverts commit afe5af1dbbc1d6e64847b2f7c0a415d20b83659e.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -702,9 +702,9 @@ function Dashboard() {
     return;
   }
   setUserRole(token.role);
-  var email = token.email;
-  var role = token.role;
-  var department = token.department;
+  const email = token.email;
+  const role = token.role;
+  const department = token.department;
 
   // Function for FA to fetch forms with year and div
   const fetchReceivedFA = async (yearToFetch, divToFetch) => {
