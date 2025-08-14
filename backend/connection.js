4b7a773f12fede99d26c2f81262b991850494bcc
GeraldoRiberia <adisankar.lalan123@gmail.com>
2025-08-14 20:19:22 +0530

    fix
    -shh

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoURI = process.env.MONGODB_URI;
 mongoose
-  .connect('mongodb+srv://Adisankar:CB1E9r7mjPV5YLpq@cluster0.3nnx8jj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .connect(mongoURI)
   .then(() => {
     console.log("Connected to DB");
   })
