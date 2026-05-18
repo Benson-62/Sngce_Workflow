db24259360864fea93ffdfd113680d034ae5c4be
Benson-62 <bensonbvarghese62@gmail.com>
2026-05-18 08:50:54 +0530

    chore: fix vite peer dependency and cors configuration

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3096;
 
 const app = express();
 app.use(cors({
-  origin : "http://187.127.139.224" // frontend url
+  origin: ["http://187.127.139.224", "http://localhost:5173"] // frontend urls
 }));
 app.use(express.json({ limit: '15mb' }));
 
